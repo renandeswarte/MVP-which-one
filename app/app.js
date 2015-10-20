5e079781e68b0b5ab6a108e15aafea16db4fe8a5
Renan Deswarte <renandeswarte@gmail.com>
2015-10-20 12:34:33 -0700

    post comments WIP

--- app/app.js
+++ app/app.js
@@ -34,6 +34,67 @@ angular.module('myApp', [
     }
 })
 
+.controller("addMessageController", ["$scope", "$firebaseObject",  
+  function($scope, $firebaseObject) {
+
+    $scope.showMessages = function() {
+      // var array = [];
+      // var msgObject;
+      var getMessages = new Firebase("https://renan-app.firebaseio.com/comments/");
+      // console.log($scope.authData.post);
+      // getMessages.orderByChild("postId").equalTo($scope.authData.post).on("child_added", function(snapshot) {
+      //   // console.log(snapshot.key());
+      //   console.log(snapshot.val());
+      //   array.push(snapshot.val());
+      // });
+      // var syncObject = $firebaseObject(getMessages);
+      // console.log("test ", array);
+      // // $scope.messagesArray = array;
+      // syncObject.$bindTo($scope, "messagesArray");
+      getMessages.orderByChild("postId").equalTo($scope.authData.post).on("value", function(snapshot) {
+        // console.log(snapshot.key());
+        console.log(snapshot.val());
+        // array.push(snapshot.val());
+        // msgObject = snapshot;
+
+        console.log(snapshot);
+        // $scope.messagesArray = snapshot;
+        snapshot.$bindTo($scope, "messagesArray");
+      });
+        // var syncObject = $firebaseObject(getMessages);
+
+      
+
+    }
+
+    $scope.addComments = function() { 
+      var commentsRef = new Firebase("https://renan-app.firebaseio.com/comments/");
+      var myDate = +new Date() //Give back millisecond
+      
+      var comment = {
+        'message': $scope.authData.userMessage,
+        'postId': $scope.authData.post,
+        'userId': $scope.authData.uid,
+        'firstname': $scope.authData.firstname,
+        'date': myDate
+      }
+      console.log(comment);
+        
+      //$save does not create a unique id, instead of push()
+      commentsRef.push(comment, function(error) { 
+        if (error) {
+          // throw error;
+          console.log(error) ;
+        } else {
+          console.log("comment saved!");
+        }
+      });
+
+      $('#message').val('');
+    };
+
+  }])
+
 
 ;
 
