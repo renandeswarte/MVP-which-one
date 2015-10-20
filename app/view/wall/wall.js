6a4e0069f829ddc3516f9089b7e8ec2affdb56ce
Renan Deswarte <renandeswarte@gmail.com>
2015-10-19 22:23:53 -0700

    Disable vote if already voted

--- app/view/wall/wall.js
+++ app/view/wall/wall.js
@@ -18,15 +18,23 @@ angular.module('myApp.wall', ['ngRoute'])
   });
 }])
 
-
 .controller('wallCtrl', ['$scope', '$firebaseObject', '$firebaseArray', function($scope, $firebaseObject, $firebaseArray) {
 
   var ref = new Firebase("https://renan-app.firebaseio.com/posts");
   var syncObject = $firebaseObject(ref);
   syncObject.$bindTo($scope, "data");
 
-  $scope.vote = function(elm, postId, side, user) {
+  $scope.hasVoted = function(postId, user) {
+    for (var vote in user.voters) {
+      if (user.voters[vote] === postId) { 
+        return true;
+      }
+    }
+    return false;
+  }
 
+  $scope.vote = function(elm, side) {
+    // Increment the votes
     elm.totalVote++;
     if (side === "one") {
       elm.voteOne++;
@@ -35,21 +43,15 @@ angular.module('myApp.wall', ['ngRoute'])
     }
   }
   $scope.updateVote = function(postId, user) {
+    // Update who voted fot this post
+    var postVotersRef = new Firebase("https://renan-app.firebaseio.com/users/" + user + "/voters");
+    var postVotersArray = $firebaseArray(postVotersRef);
+    postVotersArray.$add(postId).then(function() {
+      console.log('voter saved!');
+    }).catch(function(error) {
+      console.log('Error!');
+    });
+  }
 
+}]);
 
-      // Update who voted fot this post
-      var postVotersRef = new Firebase("https://renan-app.firebaseio.com/users/" + user + "/voters");
-      var postVotersArray = $firebaseArray(postVotersRef);
-      postVotersArray.$add(postId).then(function() {
-        console.log($scope.authData.voters)
-        console.log('voter saved!');
-      }).catch(function(error) {
-        console.log('Error!');
-      });
-
-    }
-
-
-
-  
-  }]);
